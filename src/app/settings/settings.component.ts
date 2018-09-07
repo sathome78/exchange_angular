8c6a749ae49c0028c077388056750c26d327b0ea
oleg <olegvanich@gmail.com>
2018-09-07 18:47:18 +0300

    added settings routing to TFA page

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.navigate(['/settings/two-factor-auth']);
+  }
 
   ngOnInit() {
   }
