8c7e642c27214ce15ca3e12bf7e49a6ca10f1af6
oleg <olegvanich@gmail.com>
2018-09-10 16:21:48 +0300

    add directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
+import {DropdownDirective} from './directives/dropdown.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderMobileComponent
+    HeaderMobileComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
