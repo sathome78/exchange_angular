327ad7e98fdb7953ce8b4d6acdd13ba02bc3b486
Des Des <cka3ka1231@gmail.com>
2018-09-11 10:58:26 +0300

    del header-mobile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
 import {DropdownDirective} from './directives/dropdown.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderMobileComponent,
     DropdownDirective
   ],
   imports: [
