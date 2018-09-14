0eae7b8995785b68874a63c2d150e8336319c7f0
oleg <olegvanich@gmail.com>
2018-09-14 21:02:36 +0300

    add region dropdown support

--- src/app/popups/two-factor-popup/sms-step-one/sms-step-one.component.ts
+++ src/app/popups/two-factor-popup/sms-step-one/sms-step-one.component.ts
@@ -7,14 +7,36 @@ import {OnNextStep, PopupService} from '../../../services/popup.service';
   styleUrls: ['./sms-step-one.component.scss']
 })
 export class SmsStepOneComponent implements OnInit, OnNextStep {
+  dropDownOpen: boolean;
+  region: {country: string, iconPath: string};
 
-  constructor(private popupService: PopupService){ }
+  constructor(private popupService: PopupService) {
+
+  }
 
   ngOnInit() {
+    this.region = this.getRegions().slice()[3];
   }
 
   onNextStep() {
     this.popupService.moveNextStep();
+  }
+
+  openDropdown() {
+    this.dropDownOpen = !this.dropDownOpen;
+  }
+
+  getRegions(): {country: string, iconPath: string}[] {
+    const regions: {country: string, iconPath: string}[] = [];
+    regions.push({country: 'Poland', iconPath: '../../../../assets/img/flag-pl.svg'});
+    regions.push({country: 'Russia', iconPath: '../../../../assets/img/flag-ru.svg'});
+    regions.push({country: 'USA', iconPath: '../../../../assets/img/flag-en.svg'});
+    regions.push({country: 'China', iconPath: '../../../../assets/img/flag-cn.svg'});
+    return regions;
+  }
+
+  onSelect(lRegion: {country: string, iconPath: string}) {
+    this.region = lRegion;
   }
 
 }
