234e35b512fea74336e9f034c7ecf39f66ffd165
oleg <olegvanich@gmail.com>
2018-09-14 17:48:50 +0300

    added tlg fourth and back navigation

--- src/app/popups/two-factor-popup/telegram-step-one/telegram-step-one.component.ts
+++ src/app/popups/two-factor-popup/telegram-step-one/telegram-step-one.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {OnNextStep, PopupService} from '../../../services/popup.service';
 
 @Component({
   selector: 'app-telegram-step-one',
   templateUrl: './telegram-step-one.component.html',
   styleUrls: ['./telegram-step-one.component.scss']
 })
-export class TelegramStepOneComponent implements OnInit {
+export class TelegramStepOneComponent implements OnInit, OnNextStep {
 
-  constructor() { }
+  constructor(private popupService: PopupService) { }
 
   ngOnInit() {
+  }
+
+  onNextStep() {
+    this.popupService.moveNextStep();
   }
 
 }
