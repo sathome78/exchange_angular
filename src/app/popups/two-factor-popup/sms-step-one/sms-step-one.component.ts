18688ea85e41ccdf6b53dd89246566e9f45a601d
oleg <olegvanich@gmail.com>
2018-09-14 16:04:28 +0300

    added sms fourth and back navigation

--- src/app/popups/two-factor-popup/sms-step-one/sms-step-one.component.ts
+++ src/app/popups/two-factor-popup/sms-step-one/sms-step-one.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {OnNextStep, PopupService} from '../../../services/popup.service';
 
 @Component({
   selector: 'app-sms-step-one',
   templateUrl: './sms-step-one.component.html',
   styleUrls: ['./sms-step-one.component.scss']
 })
-export class SmsStepOneComponent implements OnInit {
+export class SmsStepOneComponent implements OnInit, OnNextStep {
 
-  constructor() { }
+  constructor(private popupService: PopupService){ }
 
   ngOnInit() {
+  }
+
+  onNextStep() {
+    this.popupService.moveNextStep();
   }
 
 }
