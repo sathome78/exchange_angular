c7b7692b180d75c40b1e3692882ca6ced1c54ec4
oleg <olegvanich@gmail.com>
2018-09-14 16:01:08 +0300

    added google fourth and back navigation

--- src/app/popups/two-factor-popup/google-step-one/google-step-one.component.ts
+++ src/app/popups/two-factor-popup/google-step-one/google-step-one.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {OnNextStep, PopupService} from '../../../services/popup.service';
 
 @Component({
   selector: 'app-google-step-one',
   templateUrl: './google-step-one.component.html',
   styleUrls: ['./google-step-one.component.scss']
 })
-export class GoogleStepOneComponent implements OnInit {
+export class GoogleStepOneComponent implements OnInit, OnNextStep {
 
-  constructor() { }
+  constructor(private popupService: PopupService) { }
 
   ngOnInit() {
+
+  }
+
+  onNextStep() {
+    this.popupService.moveNextStep();
   }
 
 }
--- src/app/popups/two-factor-popup/google-step-two/google-step-two.component.ts
+++ src/app/popups/two-factor-popup/google-step-two/google-step-two.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {OnNextStep, PopupService} from '../../../services/popup.service';
 
 @Component({
   selector: 'app-google-step-two',
   templateUrl: './google-step-two.component.html',
   styleUrls: ['./google-step-two.component.scss']
 })
-export class GoogleStepTwoComponent implements OnInit {
+export class GoogleStepTwoComponent implements OnInit, OnNextStep {
 
-  constructor() { }
+  constructor(private popupService: PopupService) { }
 
   ngOnInit() {
+  }
+
+  onNextStep() {
+    this.popupService.moveNextStep();
   }
 
 }
