9ba1156ae67bd99b6d6304b12e4a5b93c7398b66
oleg <olegvanich@gmail.com>
2018-09-14 23:55:40 +0300

    added password validation

--- src/app/settings/password/password.component.ts
+++ src/app/settings/password/password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-password',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordComponent implements OnInit {
 
+  form: FormGroup;
+  passwordFirst: FormControl;
+  isPasswordVisible: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.passwordFirst = new FormControl(null, {
+      validators: [Validators.required, Validators.pattern('^((?=.*\\d)(?=.*[a-zA-Z]).{8,20})$')],
+      updateOn: 'blur'
+    });
+    this.form = new FormGroup({
+      'password_1': this.passwordFirst,
+      'password_2': new FormControl(null, {
+        validators: [Validators.required, this.unmatchingPasswords.bind(this)],
+        updateOn: 'blur'
+      }),
+    });
   }
 
+  unmatchingPasswords(password_2: FormControl): {[s: string]: boolean} {
+    if (this.passwordFirst.value === password_2.value) {
+      return null;
+    }
+    return { 'passwordsDiffer': true };
+  }
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  onSubmit() {
+
+  }
+
+
+  getInputType() {
+    return this.isPasswordVisible ? 'text' : 'password';
+  }
 }
