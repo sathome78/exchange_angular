80d7e1976c6f684e8dc128c3a9ee306099f2da85
oleg <olegvanich@gmail.com>
2018-09-18 22:21:41 +0300

    added some identity items

--- src/app/popups/identity-popup/step-one/step-one.component.ts
+++ src/app/popups/identity-popup/step-one/step-one.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-step-one',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepOneComponent implements OnInit {
 
+  @Output() onNextStep = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  moveNext() {
+    this.onNextStep.emit(2);
+  }
+
+  onSubmit() {
+    this.moveNext();
   }
 
 }
--- src/app/popups/identity-popup/step-two/step-two.component.ts
+++ src/app/popups/identity-popup/step-two/step-two.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-step-two',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepTwoComponent implements OnInit {
 
+  @Output() onNextStep = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.onNextStep.emit(3);
   }
 
 }
