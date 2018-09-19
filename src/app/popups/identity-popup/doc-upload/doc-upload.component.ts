02a260a0799573a753916ec45180051c951f4a06
oleg <olegvanich@gmail.com>
2018-09-19 18:53:10 +0300

    added file upload preview feature

--- src/app/popups/identity-popup/doc-upload/doc-upload.component.ts
+++ src/app/popups/identity-popup/doc-upload/doc-upload.component.ts
@@ -9,14 +9,56 @@ export class DocUploadComponent implements OnInit {
 
   @Output() showFile = new EventEmitter<boolean>();
   fileToUpload: File = null;
+  fileUrl: any;
+  url = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  handleFileInput(files: FileList) {
-    this.fileToUpload = files.item(0);
+  handleFileInput(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (ev: any) => {
+        this.fileUrl = event.target.result;
+
+      };
+    }
+  }
+
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      this.fileToUpload = event.target.files[0];
+
+      if (this.isFileValid()) {
+        reader.readAsDataURL(this.fileToUpload); // read file as data url
+
+        reader.onload = (ev: any) => { // called once readAsDataURL is completed
+          this.url = ev.target.result;
+        };
+      } else {
+        this.fileToUpload = null;
+      }
+
+    }
+  }
+
+  isFileValid(): boolean {
+    if (this.fileToUpload == null) {
+      return false;
+    }
+    const fileName = this.fileToUpload.name.toLowerCase();
+    return fileName.endsWith('.png'.toLowerCase())
+      || fileName.endsWith('.jpg'.toLowerCase())
+      || fileName.endsWith('.jpeg'.toLowerCase())
+      || fileName.endsWith('.tif'.toLowerCase())
+      || fileName.endsWith('.gif'.toLowerCase());
   }
 
 }
