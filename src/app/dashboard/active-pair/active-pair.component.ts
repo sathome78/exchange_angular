d83431f5b0a8238f445c7043d9d381b74a427d83
oleg <olegvanich@gmail.com>
2018-09-25 12:08:29 +0300

    added dashboard components

---
+++ src/app/dashboard/active-pair/active-pair.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-pair',
+  templateUrl: './active-pair.component.html',
+  styleUrls: ['./active-pair.component.scss']
+})
+export class ActivePairComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
