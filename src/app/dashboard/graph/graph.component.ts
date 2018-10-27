c7006d12f96ec10282e7572fedecff918feeb156
oleg <olegvanich@gmail.com>
2018-10-27 19:06:21 +0300

    added build timestamp

--- src/app/dashboard/graph/graph.component.ts
+++ src/app/dashboard/graph/graph.component.ts
@@ -28,8 +28,8 @@ export class GraphComponent extends AbstractDashboardItems implements OnInit, Af
   private _symbol: ChartingLibraryWidgetOptions['symbol'] = 'AAPL';
   private _interval: ChartingLibraryWidgetOptions['interval'] = 'D';
   // BEWARE: no trailing slash is expected in feed URL
-  private _datafeedUrl = 'https://demo_feed.tradingview.com';
-  // private _datafeedUrl = environment.apiUrl + 'info/private/v2/dashboard/history';
+  // private _datafeedUrl = 'https://demo_feed.tradingview.com';
+  private _datafeedUrl = environment.apiUrl + 'info/private/v2/dashboard/history';
   private _libraryPath: ChartingLibraryWidgetOptions['library_path'] = 'assets/js/charting_library/';
   private _chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url'] = 'https://saveload.tradingview.com';
   private _chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version'] = '1.1';
