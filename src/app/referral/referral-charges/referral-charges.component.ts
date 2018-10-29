ef2290575def8925b6e5364d63db5fdde4465311
Dmitry <fadeev.ds.94@gmail.com>
2018-10-29 17:20:02 +0200

    fix orders. Add referral components

---
+++ src/app/referral/referral-charges/referral-charges.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-referral-charges',
+  templateUrl: './referral-charges.component.html',
+  styleUrls: ['./referral-charges.component.scss']
+})
+export class ReferralChargesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
