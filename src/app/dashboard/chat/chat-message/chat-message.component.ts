4e82592e587c2e09e23f8627e7c03c743b483261
oleg <olegvanich@gmail.com>
2018-10-31 23:50:27 +0200

    added chat input support + connection +  ф

--- src/app/dashboard/chat/chat-message/chat-message.component.ts
+++ src/app/dashboard/chat/chat-message/chat-message.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ChatItem} from '../chat-item.model';
 import {AuthService} from '../../../services/auth.service';
+import {SimpleChat} from '../simple-chat.model';
 
 @Component({
   selector: 'app-chat-message',
@@ -9,7 +10,7 @@ import {AuthService} from '../../../services/auth.service';
 })
 export class ChatMessageComponent implements OnInit {
 
-  @Input() message: ChatItem;
+  @Input() message: SimpleChat;
 
   constructor(private authService: AuthService) { }
 
