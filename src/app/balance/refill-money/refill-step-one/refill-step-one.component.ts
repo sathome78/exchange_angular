bcd99170774f7f221c196f25e2189d687722f648
Dmitry <fadeev.ds.94@gmail.com>
2018-11-19 17:53:15 +0200

    add refill-crypto mob and desktop

---
+++ src/app/balance/refill-money/refill-step-one/refill-step-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-refill-step-one',
+  templateUrl: './refill-step-one.component.html',
+  styleUrls: ['./refill-step-one.component.scss']
+})
+export class RefillStepOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
