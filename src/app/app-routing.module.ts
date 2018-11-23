8b9877bd4685993899a39f635066e9b4248749b2
Dmitry <fadeev.ds.94@gmail.com>
2018-11-23 17:00:01 +0200

    add transaction history mob and desktop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,34 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {AuthGuard} from './services/auth.guard';
 import {BalanceComponent} from './balance/balance.component';
+import {TransactionHistoryComponent} from './transaction-history/transaction-history.component';
 import {OrdersHistoryComponent} from './orders/orders-history/orders-history.component';
 import {OpenOrdersComponent} from './orders/open-orders/open-orders.component';
-import {ReferralStructureComponent } from './referral/referral-structure/referral-structure.component';
+import {ReferralStructureComponent} from './referral/referral-structure/referral-structure.component';
 import {ReferralChargesComponent} from './referral/referral-charges/referral-charges.component';
 import {FinalRegistrationComponent} from './auth/final-registration/final-registration.component';
 import {SettingsComponent} from './settings/settings.component';
 
 const routes: Routes = [
   // permit all
-  { path: 'dashboard',      component: DashboardComponent },
-  { path: 'balance',      component: BalanceComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard] },
-  { path: 'open-orders', component: OpenOrdersComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
-  { path: 'final-registration/token', component: FinalRegistrationComponent },
-  { path: 'orders-history', component: OrdersHistoryComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard] },
-  { path: 'referral-structure', component: ReferralStructureComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard] },
-  { path: 'referral-charges', component: ReferralChargesComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard] },
-  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
+  {path: 'dashboard', component: DashboardComponent},
+  // {path: 'balance', component: BalanceComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
+  {path: 'balance', component: BalanceComponent},
+  // {path: 'transaction-history', component: TransactionHistoryComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
+  {path: 'transaction-history', component: TransactionHistoryComponent},
+  {path: 'open-orders', component: OpenOrdersComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
+  {path: 'final-registration/token', component: FinalRegistrationComponent},
+  {path: 'orders-history', component: OrdersHistoryComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
+  {path: 'referral-structure', component: ReferralStructureComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
+  {path: 'referral-charges', component: ReferralChargesComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
+  {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
   // { path: 'settings', loadChildren: './settings/settings.module#SettingsModule'},
   // { path: 'funds',      component: DashboardComponent, },ç
   // { path: 'orders',      component: DashboardComponent, },
   // { path: 'tfa-popup', component: TwoFactorPopupComponent },
   // { path: 'settings',  component: SettingsComponent,    loadChildren: 'app/settings/settings.module#SettingsModule' },
-
 
 
   // { path: 'login',          loadChildren: 'app/user/login/login.module#LoginModule' },
@@ -42,7 +45,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,  {onSameUrlNavigation: 'reload'})],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
