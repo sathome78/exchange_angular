57a69624dcc063cb528f05266c0d8c619fbcacde
Dmitry <fadeev.ds.94@gmail.com>
2018-11-26 12:10:20 +0200

    fix height scroll in markets desktop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,8 +14,8 @@ import {SettingsComponent} from './settings/settings.component';
 const routes: Routes = [
   // permit all
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'balance', component: BalanceComponent},
-  // {path: 'balance', component: BalanceComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
+  // {path: 'balance', component: BalanceComponent},
+  {path: 'balance', component: BalanceComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
   {path: 'transaction-history', component: TransactionHistoryComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
   {path: 'open-orders', component: OpenOrdersComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]},
   {path: 'final-registration/token', component: FinalRegistrationComponent},
