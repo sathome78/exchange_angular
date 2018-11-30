a7707bd2cffeb81f64c77ab5a27470a80f7dc1a3
Dmitry <fadeev.ds.94@gmail.com>
2018-11-30 16:48:10 +0200

    fix app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -63,47 +63,12 @@ import {FinalRegistrationComponent} from './auth/final-registration/final-regist
 import { RecoveryPassComponent } from './popups/recovery-pass/recovery-pass.component';
 import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
 import { FinalStepRecoveryPasswordComponent } from './auth/final-step-recovery-password/final-step-recovery-password.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { IdentityPopupComponent } from './popups/identity-popup/identity-popup.component';
-import { StepOneComponent } from './popups/identity-popup/step-one/step-one.component';
-import { StepTwoComponent } from './popups/identity-popup/step-two/step-two.component';
-import { StepThreeComponent } from './popups/identity-popup/step-three/step-three.component';
-import { StepFourComponent } from './popups/identity-popup/step-four/step-four.component';
-import { LoginPopupComponent } from './popups/login-popup/login-popup.component';
-import { MyDatePickerModule } from 'mydatepicker';
-import {WebcamModule} from 'ngx-webcam';
-import { WebcamComponent } from './popups/identity-popup/webcam/webcam.component';
-import { DocUploadComponent } from './popups/identity-popup/doc-upload/doc-upload.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {LangService} from './services/lang.service';
-import {UserService} from './services/user.service';
-import {ThemeService} from './services/theme.service';
-import {JwtInterceptor} from './interceptors/jwt.interceptor';
-import {AuthInterceptor} from './interceptors/auth.interceptor';
-import {QRCodeModule} from 'angular2-qrcode';
-import {GoogleAuthenticatorService} from './popups/two-factor-popup/google/google-authenticator.service';
-import { SharedModule } from './shared/shared.module';
-import {UserVerificationService} from './services/user-verification.service';
-import { OpenOrdersComponent } from './orders/open-orders/open-orders.component';
-import { OrdersHistoryComponent } from './orders/orders-history/orders-history.component';
-import {MockDataService} from './services/mock-data.service';
-import {TradingService} from './dashboard/trading/trading.service';
-import {OrdersService} from './dashboard/orders/orders.service';
-import { ReferralStructureComponent } from './referral/referral-structure/referral-structure.component';
-import { ReferralChargesComponent } from './referral/referral-charges/referral-charges.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { BalanceComponent } from './balance/balance.component';
-import { LoginPopupMobileComponent } from './popups/login-popup-mobile/login-popup-mobile.component';
-import { RegistrationMobilePopupComponent } from './popups/registration-mobile-popup/registration-mobile-popup.component';
-import {CurrencyPipe} from './shared/pipes/currency.pipe';
 import { RefillMoneyComponent } from './balance/refill-money/refill-money.component';
 import { RefillStepOneComponent } from './balance/refill-money/refill-step-one/refill-step-one.component';
 import { RefillStepTwoComponent } from './balance/refill-money/refill-step-two/refill-step-two.component';
 import { RefillStepThreeComponent } from './balance/refill-money/refill-step-three/refill-step-three.component';
 import { RefillCryptoComponent } from './balance/refill-money/refill-step-two/refill-crypto/refill-crypto.component';
 import { RefillFiatComponent } from './balance/refill-money/refill-step-two/refill-fiat/refill-fiat.component';
-
-
 
 @NgModule({
   declarations: [
