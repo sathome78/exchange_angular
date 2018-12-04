570de62539b5f3a725a05d29b83970fd0e31f4be
oleg <olegvanich@gmail.com>
2018-12-04 11:17:32 +0200

    updated envs for dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,9 +2,11 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {DEV_HOST} from '../app/services/http.utils';
+
 export const environment = {
   production: false,
-  apiUrl: DEV_,
+  apiUrl: DEV_HOST,
   encodeKey: '3255c246-4b9f-43a5-b2dd-63524f959953',
 };
 
