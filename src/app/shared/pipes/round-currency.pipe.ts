9b8e2c5937596281a60a4b3bae0ff17a8f4cbf23
gogoleu <evgeniy.gogol@upholding.biz>
2018-12-19 18:29:30 +0200

    add fix changes

--- src/app/shared/pipes/round-currency.pipe.ts
+++ src/app/shared/pipes/round-currency.pipe.ts
@@ -7,7 +7,6 @@ export class RoundCurrencyPipe implements PipeTransform {
   transform(value: number): string {
     let rounded: number | string = ((Math.floor(value * 100000000)) / 100000000);
     const hasFractionalPart = ('' + rounded).indexOf('.') >= 0;
-    debugger
     if(!hasFractionalPart) {
       rounded = rounded.toFixed(1);
     }
