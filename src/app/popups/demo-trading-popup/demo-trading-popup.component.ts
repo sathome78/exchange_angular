310907ee74e2530bce817a89a4d7cfcb06089186
Dmitry <fadeev.ds.94@gmail.com>
2018-12-21 11:31:24 +0200

    fix not-work popup

--- src/app/popups/demo-trading-popup/demo-trading-popup.component.ts
+++ src/app/popups/demo-trading-popup/demo-trading-popup.component.ts
@@ -8,18 +8,21 @@ import {PopupService} from '../../shared/services/popup.service';
 })
 export class DemoTradingPopupComponent implements OnInit {
 
-   @Input() message;
-   public showHtml;
+  @Input() message;
+  public showHtml;
 
-   private defautMessage = `<p>Unfortunately now you are not able to trade on this version of the website.</p>
+  private defautMessage = `<p>Unfortunately now you are not able to trade on this version of the website.</p>
       <p>Now you have an opportunity to test a redesigned demo version go the dashboard. After all the tests are
         conducted, the updated dashboard will be available for real trading!</p>`;
 
-   private notWork = `<p>This functionality is at the stage of development.</p> <p>These functions are available on exrates.me</p>`;
+  // private notWork = `<p>This functionality is at the stage of development.</p> <p>These functions are available on <a href="https://exrates.me" class="link">exrates.me</a></p>`;
+  private notWork = `<p>Unfortunately, for this moment there is no opportunity to continue desired action on current version of site. Please, go to <a
+href="https://exrates.me" class="link link--underline">exrates.me</a> to proceed your action.</p>`;
 
   constructor(
     private popupService: PopupService,
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     switch (this.message) {
