d41160c9a4572a4fcc96b3f8adaee8f341eca9b4
Dmitry <fadeev.ds.94@gmail.com>
2018-12-24 17:38:39 +0200

    add footer pages DEVEX-746

--- src/app/popups/demo-trading-popup/demo-trading-popup.component.ts
+++ src/app/popups/demo-trading-popup/demo-trading-popup.component.ts
@@ -18,7 +18,7 @@ export class DemoTradingPopupComponent implements OnInit {
        After all the tests are conducted, the updated dashboard will be available for real trading!</p>`;
 
   private notWork = `<p>Unfortunately, for this moment there is no opportunity to continue desired action on current version of site.
-  Please, go to <a href="https://exrates.me" class="link link--underline" target="_blank">exrates.me</a> to proceed your action.</p>`;
+  Please, go to <a href="https://exrates.me" class="link" target="_blank">exrates.me</a> to proceed your action.</p>`;
 
   constructor(
     private popupService: PopupService,
