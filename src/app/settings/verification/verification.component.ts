079562ca69a5f3c9be4dd9387b06aa46bdd14ce2
gogoleu <evgeniy.gogol@upholding.biz>
2019-01-28 15:32:47 +0200

    start kyc

--- src/app/settings/verification/verification.component.ts
+++ src/app/settings/verification/verification.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PopupService} from '../../shared/services/popup.service';
 import {UserVerificationService} from '../../shared/services/user-verification.service';
+import {SettingsService} from '../settings.service';
 
 @Component({
   selector: 'app-verification',
@@ -10,9 +11,14 @@ import {UserVerificationService} from '../../shared/services/user-verification.s
 export class VerificationComponent implements OnInit {
 
   constructor(private popupService: PopupService,
-              private verificationService: UserVerificationService) { }
+              private verificationService: UserVerificationService,
+              private settingsService: SettingsService
+              ) { }
 
   ngOnInit() {
+    // this.settingsService.getCurrentVerificationStatusKYC().subscribe(res => console.log(res));
+    // this.settingsService.getCountriesKYC().subscribe(res => console.log(res));
+    // this.settingsService.getLanguagesKYC().subscribe(res => console.log(res));
   }
 
   onOpenIdentityPopup(mode: string) {
