983de3e53c943852836caec084246dc00d2455e3
gogoleu <evgeniy.gogol@upholding.biz>
2019-01-30 17:34:14 +0200

    open verification only special users

--- src/app/settings/verification/verification.component.ts
+++ src/app/settings/verification/verification.component.ts
@@ -5,6 +5,7 @@ import {SettingsService} from '../settings.service';
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
 import {NOT_VERIFIED, LEVEL_ONE, LEVEL_TWO} from '../../shared/constants';
+import {AuthService} from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-verification',
@@ -17,18 +18,33 @@ export class VerificationComponent implements OnInit, OnDestroy {
   public LEVEL_ONE = LEVEL_ONE;
   public LEVEL_TWO = LEVEL_TWO;
   public verificationStatus = NOT_VERIFIED;
+  public pattern = 'upholding.biz'
+  public showComponent = false;
 
   constructor(private popupService: PopupService,
               private verificationService: UserVerificationService,
-              private settingsService: SettingsService
+              private authService: AuthService,
+              private settingsService: SettingsService,
               ) { }
 
   ngOnInit() {
+    this.getVerificationStatus();
+    this.popupService
+      .getKYCPopupListener()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(value => {
+        if (!value) {
+          this.getVerificationStatus();
+        }
+      });
+     this.showComponent = this.authService.getUsername().match(this.pattern) ? true : false;
+  }
+
+  getVerificationStatus() {
     this.settingsService.getCurrentVerificationStatusKYC()
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(res => {
         this.verificationStatus = res as string;
-        console.log(res)
       });
   }
 
