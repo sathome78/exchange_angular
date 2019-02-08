40b9141c92b803ba79b9883e26fff93a3ef68a0a
gogoleu <evgeniy.gogol@upholding.biz>
2019-02-08 15:31:24 +0200

    fix login

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -7,3 +7,17 @@ export const SELL = 'SELL';
 export const NOT_VERIFIED = 'NOT_VERIFIED';
 export const LEVEL_ONE = 'LEVEL_ONE';
 export const LEVEL_TWO = 'LEVEL_TWO';
+
+
+// Authentication messages
+
+export const AUTH_MESSAGES = {
+  'USER_CREDENTIALS_NOT_COMPLETE': '',
+  'USER_EMAIL_NOT_FOUND': 'user',
+  'USER_REGISTRATION_NOT_COMPLETED': '',
+  'USER_NOT_ACTIVE': 'user',
+  'REQUIRED_EMAIL_AUTHORIZATION_CODE': '',
+  'REQUIRED_GOOGLE_AUTHORIZATION_CODE': '',
+  'EMAIL_AUTHORIZATION_FAILED': '',
+  'FAILED_TO_GET_USER_TOKEN': '',
+}
