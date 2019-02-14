98b70668193fb4e7580c9c075c05b1065c1e1ed5
oleg <olegvanich@gmail.com>
2019-02-14 12:15:46 +0200

    added pre-prod

---
+++ src/environments/environment.pre-prod.ts
@@ -0,0 +1,7 @@
+import {PRE_PROD_HOST} from '../app/shared/services/http.utils';
+
+export const environment = {
+  production: true,
+  apiUrl: PRE_PROD_HOST,
+  encodeKey: '3255c246-4b9f-43a5-b2dd-63524f959953',
+};
