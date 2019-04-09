ba4f2a2197475c745127db06af2330811017fdc6
gogoleu <evgeniy.gogol@upholding.biz>
2019-04-09 18:13:14 +0300

    restore news link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -204,7 +204,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     const encodeData = btoa(JSON.stringify({
       login: this.authService.isAuthenticated()
     }));
-    window.open(`https://support.exrates.me?data=${encodeData}`);
+    window.open(`https://news.exrates.me?data=${encodeData}`);
   }
 
 // temp solution
