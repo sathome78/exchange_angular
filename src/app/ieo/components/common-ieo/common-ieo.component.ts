7b1579eeb5f663668edc9f9f70b8468917dcfcdc
gogoleu <evgeniy.gogol@upholding.biz>
2019-05-07 15:47:46 +0300

    commot ieo page

--- src/app/ieo/components/common-ieo/common-ieo.component.ts
+++ src/app/ieo/components/common-ieo/common-ieo.component.ts
@@ -11,6 +11,8 @@ import {Router} from '@angular/router';
 import {data} from '../../JSONData';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {UtilsService} from '../../../shared/services/utils.service';
+import {IEOServiceService} from '../../../shared/services/ieoservice.service';
+import {KycIEOModel} from '../../models/ieo-kyc.model';
 
 @Component({
   selector: 'app-common-ieo',
@@ -21,17 +23,24 @@ export class CommonIEOComponent implements OnInit, OnDestroy {
 
   public ieoList: IEOItem[];
   public emailForm: FormGroup;
+  public showBuyIEO = false;
   public verificationStatus: string;
   public isSubmited = false;
+  public showPolicy = false;
+  public showNoReqs = false;
   public isAuthenticated: boolean;
   public AuthSub$: Observable<boolean>;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
   public KYC_STATUS = KYC_STATUS;
+  public requirements: KycIEOModel = new KycIEOModel(null, null, null);
+  public buyIEOData;
+  public showSuccessIEO = false;
 
   constructor(
     private store: Store<State>,
     private popupService: PopupService,
     private utilsService: UtilsService,
+    private ieoService: IEOServiceService,
     private router: Router
   ) {
     this.AuthSub$ = this.store.pipe(select(fromCore.getIsAuthenticated));
@@ -52,8 +61,8 @@ export class CommonIEOComponent implements OnInit, OnDestroy {
     this.store.pipe(select(fromCore.getIEOList))
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(res => {
-        console.log(res);
-        this.ieoList = res;
+        console.log(res)
+        this.ieoList = (res as IEOItem[] || []).reverse();
       });
   }
 
@@ -65,7 +74,6 @@ export class CommonIEOComponent implements OnInit, OnDestroy {
           this.store.pipe(select(getVerificationStatus))
             .pipe(takeUntil(this.ngUnsubscribe))
             .subscribe(res => {
-              console.log(res);
               this.verificationStatus = res;
             });
         }
@@ -104,5 +112,75 @@ export class CommonIEOComponent implements OnInit, OnDestroy {
 
   get emailControl() {
     return this.emailForm.get('email');
+  }
+
+  public closeBuyIEO() {
+    this.showBuyIEO = false;
+  }
+
+  public confirmBuyIEO(amount) {
+    this.ieoService.buyTokens({
+      currencyName: this.buyIEOData.currencyName,
+      amount: amount + '',
+    })
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((res) => {
+        this.closeBuyIEO();
+        this.openSuccessIEO();
+      });
+  }
+
+  public openSuccessIEO() {
+    this.showSuccessIEO = true;
+  }
+
+  closeSuccessIEO() {
+    this.showSuccessIEO = false;
+  }
+
+  public buyIeo(ieoItem) {
+    this.buyIEOData = ieoItem;
+    if (this.isAuthenticated) {
+      this.checkKYCStatus(ieoItem.id);
+    }
+  }
+
+  checkKYCStatus(id) {
+    this.ieoService.checkKYC(id)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((res: KycIEOModel) => {
+        if (res) {
+          this.requirements = res;
+          this.openBuyPopup();
+        }
+      });
+  }
+
+  openBuyPopup() {
+    if (!this.requirements.kycCheck) {
+      this.toggleNoReqs(true);
+    } else if (!this.requirements.policyCheck) {
+      this.togglePolicy(true);
+    } else {
+      this.showBuyIEO = true;
+    }
+  }
+
+  togglePolicy(flag: boolean) {
+    this.showPolicy = flag;
+  }
+
+  toggleNoReqs(flag: boolean) {
+    this.showNoReqs = flag;
+  }
+
+  agreeWithPolicy() {
+    this.ieoService.setPolicy()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((res) => {
+        this.togglePolicy(false);
+        this.requirements = {...this.requirements, policyCheck: true};
+        this.openBuyPopup();
+      });
   }
 }
