07c1c273cf4eace81dc563c54a57bebe07e51159
Serhii Ziatko <serhii.ziatko.work@gmail.com>
2019-05-13 14:15:42 +0300

    DEVEX-1595 fixed

--- src/app/funds/store/reducers/funds.reducer.ts
+++ src/app/funds/store/reducers/funds.reducer.ts
@@ -156,6 +156,8 @@ export function reducer(state: State = INIT_STATE, action: fromActions.Actions) 
 
     case fromActions.LOAD_TRANSACTIONS_HISTORY:
       return {...state, loading: true};
+    case fromActions.LOAD_LAST_TRANSACTIONS_HISTORY:
+      return {...state, loading: true};
     case fromActions.SET_TRANSACTIONS_HISTORY:
       return {
         ...state,
