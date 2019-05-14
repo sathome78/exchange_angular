9bbc5ca3474224ef7937c6e57b449faf1f46309b
Serhii Ziatko <serhii.ziatko.work@gmail.com>
2019-05-14 12:34:02 +0300

    fixed buy popup

--- src/environments/environment.pre-prod.ts
+++ src/environments/environment.pre-prod.ts
@@ -3,6 +3,6 @@ import {PRE_PROD_HOST} from '../app/shared/services/http.utils';
 export const environment = {
   production: true,
   apiUrl: PRE_PROD_HOST,
-  showContent: false,
+  showContent: true,
   encodeKey: '3255c246-4b9f-43a5-b2dd-63524f959953',
 };
