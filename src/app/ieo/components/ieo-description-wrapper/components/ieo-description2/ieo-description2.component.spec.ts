b2975892161116c278a370cbc7e45ef079f59eb6
Serhii Ziatko <serhii.ziatko.work@gmail.com>
2019-05-22 12:12:52 +0300

    added components

---
+++ src/app/ieo/components/ieo-description-wrapper/components/ieo-description2/ieo-description2.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IeoDescription2Component } from './ieo-description2.component';
+
+describe('IeoDescription2Component', () => {
+  let component: IeoDescription2Component;
+  let fixture: ComponentFixture<IeoDescription2Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ IeoDescription2Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IeoDescription2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
