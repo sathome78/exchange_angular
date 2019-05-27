fd3ac736b3a3ba6d1e8c6c0f0ee012c2d6801936
gogoleu <evgeniy.gogol@upholding.biz>
2019-05-27 17:40:04 +0300

    task 2309

--- src/app/static-pages/static-pages.module.ts
+++ src/app/static-pages/static-pages.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { StaticPagesComponent } from "./static-pages/static-pages.component";
-import { AboutUsComponent } from "./about-us/about-us.component";
-import { ContactsComponent } from "./contacts/contacts.component";
-import { PrivacyPolicyComponent } from "./privacy-policy/privacy-policy.component";
-import { TermsAndConditionsComponent } from "./terms-and-conditions/terms-and-conditions.component";
-import { StaticPagesRoutingModule } from "./static-pages.routing";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StaticPagesComponent } from './static-pages.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
+import { StaticPagesRoutingModule } from './static-pages.routing';
 import {HttpClient} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {translateInfo} from '../shared/configs/translate-options';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {StaticPagesService} from './static-pages.service';
+import {ContactsService} from './services/contacts.service';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, translateInfo.path.staticPages, translateInfo.suffix);
@@ -39,9 +39,8 @@ export function createTranslateLoader(http: HttpClient) {
     TermsAndConditionsComponent
   ],
   providers: [
-    StaticPagesService
+    ContactsService
   ]
 })
 export class StaticPagesModule {
-
 }
