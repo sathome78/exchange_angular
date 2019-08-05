d1109953ee57cace3386c7dba1a6dfb8a7788b12
Serhii Ziatko <serhii.ziatko@gmail.com>
2019-08-06 01:35:01 +0300

    DEVEX-3212 fixed

--- src/app/funds/balance/refill-money/refill-step-three/refill-step-three.component.ts
+++ src/app/funds/balance/refill-money/refill-step-three/refill-step-three.component.ts
@@ -5,6 +5,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Store, select } from '@ngrx/store';
 import * as fromCore from '../../../../core/reducers';
 import { Subject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-refill-step-three',
@@ -15,9 +16,15 @@ export class RefillStepThreeComponent implements OnInit {
   form: FormGroup;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
   googleAuthenticator: any;
-  statusMessage: string = '';
+  statusMessage = '';
+  twoFaTitle = '';
+  isError = false;
 
-  constructor(public balanceService: BalanceService, private store: Store<fromCore.State>) {
+  constructor(
+    public balanceService: BalanceService,
+    private store: Store<fromCore.State>,
+    public translateService: TranslateService
+  ) {
     balanceService
       .getRefillTransfer()
       .pipe(first())
@@ -35,8 +42,15 @@ export class RefillStepThreeComponent implements OnInit {
       .pipe(select(fromCore.getGAStatus))
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((GA: any) => {
-        console.log(GA);
+        // console.log(GA);
         this.googleAuthenticator = GA;
+        if (this.googleAuthenticator) {
+          this.twoFaTitle = this.translateService.instant('Use Google Authenticator to generate pincode');
+        } else {
+          this.twoFaTitle = this.translateService.instant(
+            'Please enter two-factor authentication code that was sent to your email'
+          );
+        }
       });
   }
 
@@ -70,9 +84,21 @@ export class RefillStepThreeComponent implements OnInit {
   }
 
   setStatusMessage(err) {
-    if (err['status'] === 400) {
+    if (err['status'] === 400 && err.error.cause === 'IncorrectPinException') {
       this.form.reset();
-      this.statusMessage = 'Wrong code';
+      this.isError = true;
+      if (this.googleAuthenticator) {
+        this.twoFaTitle = this.translateService.instant(
+          'Code is wrong! Please, check you code in Google Authenticator application.'
+        );
+      } else {
+        this.twoFaTitle = this.translateService.instant('Code is wrong! New code was sent to your email.');
+      }
+    } else if (err['status'] === 400 && err.error.cause === 'RequestLimitExceededException') {
+      this.isError = true;
+      this.twoFaTitle = this.translateService.instant(err.error.details);
+    } else {
+      console.error(err);
     }
   }
 }
