612006c3eb664c6c105d8b7ed5f572f299efab69
Serhii Ziatko <serhii.ziatko@gmail.com>
2019-08-19 11:25:15 +0300

    DEVEX-3715 added feature

--- src/app/dashboard/services/isFavorite.pipe.ts
+++ src/app/dashboard/services/isFavorite.pipe.ts
@@ -3,7 +3,7 @@ import { CurrencyPair } from 'app/model';
 
 @Pipe({ name: 'isFavoritePipe' })
 export class IsFavoritePipe implements PipeTransform {
-  transform(pair: CurrencyPair, userFavorites: Array<number> = []): any {
+  transform(pair: CurrencyPair, userFavorites: number[] = []): any {
     return userFavorites.indexOf(pair.currencyPairId) >= 0;
   }
 }
