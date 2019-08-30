cec5b2d8b248752689625fb0be203475d1010f29
Sergey Zyatko <sergey.zyatko@upholding.biz>
2019-08-30 12:09:16 +0200

    Merge pull request #255 in MAIN/exrates_angular from DEVEX-3702-qiwi-qiwi-stg to staging
    Squashed commit of the following:
    
    commit 84a0021abebae09829eb7b92b78f3a3e328a1fa2
    Merge: 5cf07d3f 5ecca381
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Fri Aug 30 13:00:57 2019 +0300
    
        Merge branch 'DEVEX-3702-qiwi-qiwi-stg' of ssh://git.listumbrella.com:8979/main/exrates_angular into DEVEX-3702-qiwi-qiwi-stg
    
    commit 5cf07d3fcc26371f2985b28685372da30e4eba20
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Fri Aug 30 13:00:15 2019 +0300
    
        DEVEX-3702 hidden input for QIWI
    
    commit 5c82a89be37f7b9be3857eb022a61066f39019aa
    Merge: fe3cc71e 92d9a91f
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Fri Aug 30 12:17:27 2019 +0300
    
        Merge branch 'staging' of ssh://git.listumbrella.com:8979/main/exrates_angular into DEVEX-3702-qiwi-qiwi-stg
    
    commit 5ecca38107cc5aea1330234e09feb97a1564f394
    Author: Alex <lex6792@yandex.ru>
    Date:   Thu Aug 29 17:42:05 2019 +0300
    
        add qiwi style
    
    commit d41df4081fe4650c02858b4250296e584e8ea169
    Merge: fe3cc71e c909fba6
    Author: Alex <lex6792@yandex.ru>
    Date:   Thu Aug 29 15:17:06 2019 +0300
    
        Merge remote-tracking branch 'origin/staging' into DEVEX-3702-qiwi-qiwi-stg
    
    commit fe3cc71eaf569ef89121844a3ba0312df2402fc4
    Merge: 410d041a dc81fb00
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Wed Aug 28 10:08:20 2019 +0300
    
        merged with staging
    
    commit 410d041aaf7ff372c28fc399ffa623ef80c557bc
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Wed Aug 21 13:40:31 2019 +0300
    
        DEVEX-3702 added functionality

--- src/environments/environment.pre-prod.ts
+++ src/environments/environment.pre-prod.ts
@@ -3,7 +3,7 @@ import { PRE_PROD_HOST } from '../app/shared/services/http.utils';
 export const environment = {
   production: true,
   apiUrl: PRE_PROD_HOST,
-  showContent: false,
+  showContent: true,
   captcha: true,
   encodeKey: '3255c246-4b9f-43a5-b2dd-63524f959953',
 };
