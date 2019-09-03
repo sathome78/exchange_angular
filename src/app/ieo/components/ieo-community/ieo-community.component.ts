6d26971f0045695c0006dceddb6b530d00f4998b
Serhii Ziatko <serhii.ziatko@gmail.com>
2019-09-03 12:53:04 +0300

    DEVEX-3887 fixed website links

--- src/app/ieo/components/ieo-community/ieo-community.component.ts
+++ src/app/ieo/components/ieo-community/ieo-community.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { IEOItem } from 'app/model/ieo.model';
 
 @Component({
   selector: 'app-ieo-community',
@@ -7,11 +8,32 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IEOCommunityComponent implements OnInit {
-  @Input() public community = null;
+  public _IEOName: string;
+  @Input('IEOName')
+  get IEOName(): string {
+    return this._IEOName;
+  }
+  set IEOName(value: string) {
+    this._IEOName = value;
+    this.website = this.selectWebsite(value);
+  }
+  public website = null;
 
   constructor() {}
 
   ngOnInit() {}
+
+  selectWebsite(name) {
+    if (!name) {
+      return null;
+    }
+    switch (name) {
+      case 'DOM':
+        return 'https://debourse.com';
+      default:
+        return null;
+    }
+  }
 
   trackByCommunity(index) {
     return index;
