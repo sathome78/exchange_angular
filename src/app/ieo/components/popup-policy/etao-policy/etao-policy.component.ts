25eb5cb9c1602f8c4adef6d1cbe5eb400992e4dd
Sergey Zyatko <sergey.zyatko@upholding.biz>
2019-09-04 18:15:00 +0200

    Merge pull request #315 in MAIN/exrates_angular from feature/DEVEX-3917-ieo-master to master
    Squashed commit of the following:
    
    commit d91835707488c727e56c1e1e91497f95ba94b4bf
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Wed Sep 4 19:00:38 2019 +0300
    
        DEVEX-3794 finished
    
    commit a670b9aa3c039c73d658d89d15109e58be1ab8d7
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Tue Sep 3 12:59:02 2019 +0300
    
        DEVEX-3887 removed unused ngIf
    
    commit 12dbaffe483e2cc5710157880fc70bbe65b3b58c
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Tue Sep 3 12:53:04 2019 +0300
    
        DEVEX-3887 fixed website links
    
    commit 4e0f98f203914de0ba0b2241cbed59728cc01b77
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Wed Sep 4 17:57:21 2019 +0300
    
        DEVEX-3917 fixed countries
    
    commit 4697720bd1eede4de7ea72261d573d8155c9e612
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Wed Sep 4 16:21:13 2019 +0300
    
        DEVEX-3917 added text
    
    commit c975c30d34106a620ea724ce24de1069414b61bb
    Author: Serhii Ziatko <serhii.ziatko@gmail.com>
    Date:   Wed Sep 4 15:37:56 2019 +0300
    
        DEVEX-3917 added policy components

---
+++ src/app/ieo/components/popup-policy/etao-policy/etao-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-etao-policy',
+  templateUrl: './etao-policy.component.html',
+  styleUrls: ['./etao-policy.component.scss']
+})
+export class EtaoPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
