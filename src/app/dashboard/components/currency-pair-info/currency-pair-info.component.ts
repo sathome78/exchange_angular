8c91077dabbf5b1f651f74fa09647b0af8ba5b75
Serhii Ziatko <serhii.ziatko@gmail.com>
2019-09-13 12:54:32 +0300

    info-bar fixed

--- src/app/dashboard/components/currency-pair-info/currency-pair-info.component.ts
+++ src/app/dashboard/components/currency-pair-info/currency-pair-info.component.ts
@@ -2,22 +2,18 @@ import {
   Component,
   OnInit,
   OnDestroy,
-  HostListener,
-  Renderer2,
   ChangeDetectionStrategy,
   ChangeDetectorRef
 } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { takeUntil } from 'rxjs/internal/operators';
 import { select, Store } from '@ngrx/store';
-import { CurrencyPair } from 'app/model/currency-pair.model';
 import { UserBalance } from 'app/model/user-balance.model';
 import {
   State,
   getActiveCurrencyPair,
   getUserBalance,
   getCurrencyPairInfo,
-  getCurrencyPairArray,
   getSimpleCurrencyPairsSelector
 } from 'app/core/reducers/index';
 import { DashboardWebSocketService } from '../../dashboard-websocket.service';
@@ -136,26 +132,10 @@ export class CurrencyPairInfoComponent implements OnInit, OnDestroy {
 
   flagForArrow(s: string) {
     if (s === 'up') {
-      return this.currentCurrencyInfo ? this.currentCurrencyInfo.percentChange > 0 : false;
+      return this.currentCurrencyInfo ? this.currentCurrencyInfo.percentChange >= 0 : false;
     }
     if (s === 'down') {
       return this.currentCurrencyInfo ? this.currentCurrencyInfo.percentChange < 0 : false;
-    }
-    return false;
-  }
-
-  lastPriceDir(s: string) {
-    if (s === 'up') {
-      return (
-        this.currentCurrencyInfo &&
-        this.currentCurrencyInfo.currencyRate > this.currentCurrencyInfo.lastCurrencyRate
-      );
-    }
-    if (s === 'down') {
-      return (
-        this.currentCurrencyInfo &&
-        this.currentCurrencyInfo.currencyRate < this.currentCurrencyInfo.lastCurrencyRate
-      );
     }
     return false;
   }
