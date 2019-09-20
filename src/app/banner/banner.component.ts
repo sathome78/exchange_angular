ca74c9299d8c0962dcdffb61ca24f1d742f6014f
Alex <lexesss64@gmail.com>
2019-09-20 16:47:01 +0300

    add banner carousel

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+  public slides = [];
+
+  public slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    vertical: true
+
+  };
+  constructor() {
+
+    
+   }
+
+  ngOnInit() {
+  }
+
+}
